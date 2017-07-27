fa58db4c19bedc2cf7392265ac73c3e80f08f263
galanisd <galanisd@gmail.com>
2017-07-27 12:57:12 +0300

    Added TopicsRegistry

---
+++ src/main/java/eu/openminted/messageservice/connector/TopicsRegistry.java
@@ -0,0 +1,7 @@
+package eu.openminted.messageservice.connector;
+
+public class TopicsRegistry {
+	
+	public final static String workflowsEditor = "workflows.editor";
+	public final static String workflowsExecution = "workflows.execution";
+}
